0f92ce3f633c31744680d3e6d9901bf6a81cf593
Joe Izang <joeizang@hotmail.com>
2019-09-17 12:21:00 +0100

    converted solution configuration options to an object.

--- tripodbackend/src/config/authentication.ts
+++ tripodbackend/src/config/authentication.ts
@@ -2,16 +2,19 @@ import * as jwt from 'express-jwt'
 import * as jwksRsa from 'jwks-rsa'
 import * as jwtauth from 'express-jwt-authz'
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+const config = {
+  checkJwt : jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: `https://${process.env.AUTH0_DOMAIN}`,
+    algorithms: ['RS256']
   }),
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}`,
-  algorithms: ['RS256']
-})
+  JwtAuth: jwtauth,
+}
 
-export { checkJwt, jwtauth }
+export { config }
--- tripodbackend/src/controllers/authorizeController.ts
+++ tripodbackend/src/controllers/authorizeController.ts
@@ -1,11 +1,18 @@
 import { Router, Request, Response } from 'express'
-import { checkJwt, jwtauth } from '../config/authentication'
+import { config } from '../config/authentication'
 
 const router = Router()
 
+/**
+ * Authenticate users when they get to this endpoint
+ * 
+ * @param {jwt.RequestHandler} checks and makes sure that jwt toke in valid
+ * @param {Express.Request} bears all artifacts of a request made to the api
+ * @param {Express.Response} carries the payload of the servers response
+ */
 
-router.get('/auth', checkJwt, jwtauth, (req: Request, res: Response) => {
-  // two things need to happen here, if the user is not properly authenticated then they should
+router.get('/auth', config.checkJwt, (req: Request, res: Response) => {
+  // two things need to happen here, if the user is not authenticated then they should
   // be redirected to proper endpoint at auth0 to get properly authenticated - this is the default behaviour
 
   //second would be that if they are already loggedin then they should be sent on their merry way
